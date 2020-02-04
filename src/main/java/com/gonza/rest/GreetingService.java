63560a54d93b5fd54dfd6915ad9dbf9b9d0e1959
Izquierdo Heras <goio@gft.com>
2020-02-04 16:32:55 +0100

    se deja como antes, pero con las pruebas comentadas

--- src/main/java/com/gonza/rest/GreetingService.java
+++ src/main/java/com/gonza/rest/GreetingService.java
@@ -26,7 +26,7 @@ public class GreetingService {
 	public Response getMsg(@PathParam("param") String msg) {
  
 		//String output = "Jersey say : " + msg;
-		
+		System.out.println("Entrada getMsg");
 		Greeting out=new Greeting("1", "HOla DAvid");
 		final Gson gson = new Gson();
 		final String representacionJSON = gson.toJson(out);
@@ -38,7 +38,7 @@ public class GreetingService {
 	@GET
 	@Produces("application/json")
 	public Response getGreetings() {
- 
+		System.out.println("Entrada getGreetings");
 		List<Greeting> out=new ArrayList<Greeting>();		
 		out.add(new Greeting("1", "HOla DAvid"));
 		out.add(new Greeting("2", "HOla Valde"));
@@ -59,7 +59,7 @@ public class GreetingService {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response createGreetings(String jsonRequest) {
-		
+		System.out.println("Entrada createGreetings");
 		final Gson gson = new Gson();
 		Greeting greeting=gson.fromJson(jsonRequest, Greeting.class);
 		
